85287b84e245088044143278ee21204298fb8dc7
eiji-hb <eijisugiurar@gmail.com>
2020-07-10 23:50:52 +0900

    500error

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+Route::get('/', function () {
+  return view('welcome');
+});
 
-
-Route::get('/', 'HomeController@index')->name('root');
+// Route::get('/', 'HomeController@index')->name('root');
 Route::get('show/{id}', 'HomeController@index')->name('home.show');
 Route::get('movie', 'HomeController@index')->name('home.movie');
 Route::get('series', 'HomeController@index')->name('home.series');
