ae1b5b8b4051d9856f8491fe2d5c06801e97e295
eiji-hb <eijisugiurar@gmail.com>
2020-07-10 22:17:30 +0900

    [add]imgデータ

--- database/seeds/ImgSeeder.php
+++ database/seeds/ImgSeeder.php
@@ -12,6 +12,49 @@ class ImgSeeder extends Seeder
      */
     public function run()
     {
-        //
+      $curl = curl_init();
+      $videos = App\Models\Video::all();
+      $nfids = $videos->pluck('nfid','id');
+      foreach($nfids as $id=>$nfid){
+        $video =  App\Models\Video::find($id);
+        // echo $id.PHP_EOL;
+        if($video->img){
+          echo "id:".$id."保存済み".PHP_EOL;
+        }else{
+          curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://unogsng.p.rapidapi.com/images?netflixid=".$nfid,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+              "x-rapidapi-host: ".config('services.net.host'),
+              "x-rapidapi-key: ".config('services.net.key'),
+            ),
+          ));
+          $response = curl_exec($curl);
+          $err = curl_error($curl);
+          if ($err) {
+            echo "cURL Error #:" . $err;
+          } else {
+            $data = json_decode($response, true);
+            for($i = 0; $i < count($data["results"]);$i++){
+              if($data["results"][$i]["itype"] === "bo342x684"){
+                $video->img = $data["results"][$i]["url"];
+                $video->save();
+                echo "id:".$id."_saved".PHP_EOL;
+                break;
+                sleep(1);
+              }
+            }
+          }
+        }
+        echo "残り: ".$id."/".count($nfids).PHP_EOL;
+      }
+      echo "---終了---".PHP_EOL;
+      curl_close($curl);
     }
 }
--- resources/views/homes/index.blade.php
+++ resources/views/homes/index.blade.php
@@ -4,7 +4,13 @@
 <div class="container">
   <div class="row">
     @foreach($videos as $video)
-      {{$video->title}} <br>
+      <div class="col-12 col-md-6 col-lg-3 p-2">
+        @if ($video->img === null)
+          <img src="{ asset('storage/noimage.png') }}">
+        @else
+          <img src={{$video->img}}>
+        @endif
+      </div>
     @endforeach
   </div>
 </div>
