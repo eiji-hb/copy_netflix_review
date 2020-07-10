9144c3f719a2370852132a347acd0e83cb6c7020
eiji-hb <eijisugiurar@gmail.com>
2020-07-11 00:04:34 +0900

    500error

--- resources/views/homes/index.blade.php
+++ resources/views/homes/index.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
-
+index
 @section('content')
 <div class="container">
   <div class="row">
     @foreach($videos as $video)
       <div class="col-12 col-md-6 col-lg-3 p-2">
 
-          <img src={{$video->img}}>
+
 
       </div>
     @endforeach
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-Route::get('/', function () {
-  return view('welcome');
-});
+// Route::get('/', function () {
+//   return view('welcome');
+// });
 
-Route::get('index', 'HomeController@index')->name('root');
+Route::get('/', 'HomeController@index')->name('root');
 Route::get('show/{id}', 'HomeController@index')->name('home.show');
 Route::get('movie', 'HomeController@index')->name('home.movie');
 Route::get('series', 'HomeController@index')->name('home.series');
