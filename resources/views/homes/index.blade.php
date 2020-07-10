f856e8077a453683e6df27f456e042577372b757
eiji-hb <eijisugiurar@gmail.com>
2020-07-10 23:33:15 +0900

    [modify]view

--- resources/views/homes/index.blade.php
+++ resources/views/homes/index.blade.php
@@ -5,11 +5,9 @@
   <div class="row">
     @foreach($videos as $video)
       <div class="col-12 col-md-6 col-lg-3 p-2">
-        @if ($video->img === null)
-          <img src="{ asset('storage/noimage.png') }">
-        @else
+
           <img src={{$video->img}}>
-        @endif
+
       </div>
     @endforeach
   </div>
