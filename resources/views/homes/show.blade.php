1c310db15bf79ca1661f0944ad220ecff8949dde
eiji-hb <eijisugiurar@gmail.com>
2020-07-11 14:48:19 +0900

    [add]show

--- resources/views/homes/show.blade.php
+++ resources/views/homes/show.blade.php
@@ -1,5 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-
+  <div class="container pt-5">
+    <div class="row">
+      <div class="col-sm-4">
+        <img src={{$video->img}}>
+      </div>
+      <div class="col-sm-8">
+        <!-- <h1>{{ $video->jp_title }}</h1> -->
+        <h5>{{ $video->title }}</h5>
+        <span class="text_muted">&#40;{{$video->year}}年制作&#41</span>
+        <p>Netflix追加日: {{date('Y',strtotime($video->titledate))}}年{{date('n',strtotime($video->titledate))}}月{{date('d',strtotime($video->titledate))}}日</p>
+        <div class="vtype">
+          @switch($video->vtype)
+            @case("movie")
+              <p>映画</p>
+              @break
+            @case("series")
+              <p>ドラマ</p>
+              @break
+            @default
+              <p>{{$video->vtype}}</p>
+          @endswitch
+        </div>
+        <div class="synopsis">
+          <p class="mb-0 font-weight-bold">あらすじ</p>
+          <p>{{$video->synopsis}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
