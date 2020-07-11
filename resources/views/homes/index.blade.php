f7ba132f5d951e2f668cff67cb86cadce56a6638
eiji-hb <eijisugiurar@gmail.com>
2020-07-11 15:49:39 +0900

    error

--- resources/views/homes/index.blade.php
+++ resources/views/homes/index.blade.php
@@ -6,7 +6,7 @@
     @foreach($videos as $video)
       <div class="col-12 col-md-6 col-lg-3 p-2">
         @if ($video->img === null)
-          <img src="{ asset('storage/noimage.png') }">
+
         @else
           <a href="{{route('homes.show',['id' => $video->id])}}"><img src={{$video->img}}></a>
         @endif
