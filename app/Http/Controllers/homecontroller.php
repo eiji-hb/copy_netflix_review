c34df91c08638a12b06d0892568ac3153770965c
eiji-hb <eijisugiurar@gmail.com>
2020-07-11 21:31:01 +0900

    [add]user

--- app/Http/Controllers/homecontroller.php
+++ app/Http/Controllers/homecontroller.php
@@ -4,26 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Video;
-
-class homecontroller extends Controller
+class HomeController extends Controller
 {
-  public function index()
-  {
-    $videos = Video::all();
-    return view('homes.index',['videos'=>$videos]);
-  }
-  public function show($id)
-  {
-    $video = Video::find($id);
-    // dd($video);
-    return view('homes.show',['video'=>$video]);
-  }
-  public function movie()
-  {
-    return view('homes.movie');
-  }
-  public function series()
-  {
-    return view('homes.series');
-  }
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+      $videos = Video::all();
+      return view('homes.index',['videos'=>$videos]);
+    }
+    public function show($id)
+    {
+      $video = Video::find($id);
+      // dd($video);
+      return view('homes.show',['video'=>$video]);
+    }
+    public function movie()
+    {
+      return view('homes.movie');
+    }
+    public function series()
+    {
+      return view('homes.series');
+    }
 }
